ea3a2c9354c9c582419e7f038de2bd0b215db210
Samuel Iwelumo <samueliwelumo@gmail.com>
2026-09-04 09:01:12 +0100

    fix: update IP extraction logic to correctly identify original client IP from the leftmost header value

--- api/common/tests_middleware.py
+++ api/common/tests_middleware.py
@@ -21,7 +21,7 @@ class AdminIPAllowlistMiddlewareTests(TestCase):
         
     @override_settings(DEBUG=False, ADMIN_ALLOWED_IPS=['192.168.1.5'])
     def test_admin_allowed_with_x_forwarded_for(self):
-        response = self.client.get('/admin/', HTTP_X_FORWARDED_FOR='10.0.0.2, 192.168.1.5')
+        response = self.client.get('/admin/', HTTP_X_FORWARDED_FOR='192.168.1.5, 10.0.0.2')
         self.assertNotEqual(response.status_code, 403)
 
     @override_settings(DEBUG=True, ADMIN_ALLOWED_IPS=['10.0.0.1'])
